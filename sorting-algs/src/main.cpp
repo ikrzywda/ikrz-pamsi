2f25778a6115216d13cfb8e31a0e546d5c8ca57c
ikrzywda <krzywdaigor890@gmail.com>
2024-04-18 21:58:11 +0200

    feature: quicksort

--- sorting-algs/src/main.cpp
+++ sorting-algs/src/main.cpp
@@ -1,31 +1,43 @@
 #include "csv/include/reader.hpp"
 
 #include <iostream>
+#include <charconv>
+#include <string_view>
+#include <vector>
+
+#include "algs/src/quicksort.cpp"
 
 int main() {
-  auto reader = read_csv("sample-data/projekt2_dane.csv");
-  if (!reader.has_value()) {
-    std::cout << "Error reading CSV file" << std::endl;
-    std::cout << "Current working directory: " << std::filesystem::current_path() << std::endl;
-    return 1;
+  // auto reader = read_csv("sample-data/projekt2_dane.csv");
+  // if (!reader.has_value()) {
+  //   std::cout << "Error reading CSV file" << std::endl;
+  //   std::cout << "Current working directory: " << std::filesystem::current_path() << std::endl;
+  //   return 1;
+  // }
+
+  // int status_code = filter_csv(reader.value(), "rating", [](const std::string &name) {
+  //   float rating;
+  //   if (sscanf(name.c_str(), "%f", &rating) != 1) {
+  //     return false;
+  //   }
+  //   return rating > 4.5;
+  // });
+
+  // if (status_code != 0) {
+  //   std::cout << "Error filtering CSV" << std::endl;
+  //   return 1;
+  // }
+
+  // std::ostream &stream = std::cout;
+  // reader.value().dump_to_stream(stream);
+
+  std::vector<int> dupa = {213,123,4,23,4,5,12,41,2,41,5,534,423,23};
+
+  quicksort<int>(dupa, 0, dupa.size() - 1);
+
+  for (auto el : dupa) {
+    std::cout << el << ' ';
   }
-
-  int status_code = filter_csv(reader.value(), "rating", [](const std::string &name) {
-    try {
-      float raitng = std::stof(name);
-      return raitng > 4.5;
-    } catch (const std::invalid_argument &e) {
-      return false;
-    }
-  });
-
-  if (status_code != 0) {
-    std::cout << "Error filtering CSV" << std::endl;
-    return 1;
-  }
-
-  std::ostream &stream = std::cout;
-  reader.value().dump_to_stream(stream);
 
   return 0;
 }
